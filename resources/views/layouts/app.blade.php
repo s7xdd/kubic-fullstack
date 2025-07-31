6038ba59fe3a91a75f6b7eb91a85223aa16d7731
Mohammed Shamil <kmshamil123@gmail.com>
2025-07-31 15:21:44 +0400

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/svg" href="{{ asset('assets/img/favicon.ico') }}">
+    <link rel="icon" type="image/svg" href="{{ get_setting('site_icon') }}">
     <title>@yield('title', config('app.name', 'Kubik'))</title>
     {!! SEO::generate() !!}
 
